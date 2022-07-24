a616e0f95b8eb8c5bd236d795b0f0f785ae58460
Rafi <rafi.putra2000@gmail.com>
2022-07-24 17:31:54 +0700

    level 2 encrypt with mongoose modul

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 
 const app = express();
@@ -17,6 +18,9 @@ const userSchema = new mongoose.Schema ({
     email: String,
     password: String
 });
+
+let secret = "thisisoutlittlesecerts."
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
 
 const User = mongoose.model("User", userSchema);
 
