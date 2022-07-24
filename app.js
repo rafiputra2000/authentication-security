aaee6c85257d70e21595467a80a2d74c25fd23ea
Rafi <rafi.putra2000@gmail.com>
2022-07-24 17:28:32 +0700

    Level 1 username and password

---
+++ app.js
@@ -0,0 +1,91 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema ({
+    email: String,
+    password: String
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.get('/', function(req, res){
+    res.render("home");
+
+});
+
+app.get('/login', function(req, res){
+    res.render("login");
+
+});
+
+app.get('/register', function(req, res){
+    res.render("register");
+
+});
+
+app.post('/register', function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save(function(err){
+        if(!err){
+            res.render("secrets");
+        } else {
+            console.log(err);
+        }
+    })
+});
+
+app.post('/login', function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUser){
+        if(!err){
+            if(foundUser){
+                if(foundUser.password === password){
+                    res.render("secrets")
+                } else {
+                    //alert("Password is wrong please correct your password quickly");
+                }
+            } else {
+                //alert(username + " username is wrong please check your username again");
+            }
+        } else {
+            console.log(err);
+        }
+
+    });
+
+});
+
+app.listen(3000, function(err){
+    if(!err){
+        console.log("Server running on port 3000");
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
